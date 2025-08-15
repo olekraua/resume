acfc58d4789056bd04fd7e11f784a4a313709f9b
olekraua <ksanya7@gmail.com>
2025-08-15 10:19:03 +0200

    Commit

--- src/main/java/net/devstudy/resume/model/CurrentProfile.java
+++
@@ -1,9 +0,0 @@
-package net.devstudy.resume.model;
-
-public class CurrentProfile {
-    private Long id;
-    public CurrentProfile() {}
-    public CurrentProfile(Long id) { this.id = id; }
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-}
--- src/main/java/net/devstudy/resume/service/EditProfileService.java
+++
@@ -1,8 +0,0 @@
-package net.devstudy.resume.service;
-
-import net.devstudy.resume.domain.Profile;
-import net.devstudy.resume.form.SignUpForm;
-
-public interface EditProfileService {
-    Profile createNewProfile(SignUpForm form);
-}
