f16bda2c7c455a6d20717704bbb42a16deb30d9e
olekraua <ksanya7@gmail.com>
2025-09-27 14:18:44 +0200

    Commit

--- src/main/java/net/devstudy/resume/domain/Profile.java
+++ src/main/java/net/devstudy/resume/domain/Profile.java
@@ -1,6 +1,11 @@
 package net.devstudy.resume.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "profile")
