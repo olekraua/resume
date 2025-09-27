616bc090d9ebd094e1497f09c10eea93307fea09
olekraua <ksanya7@gmail.com>
2025-09-27 14:35:03 +0200

    Commit

--- src/main/java/net/devstudy/resume/repository/ProfileRepository.java
+++ src/main/java/net/devstudy/resume/repository/ProfileRepository.java
@@ -3,7 +3,8 @@ package net.devstudy.resume.repository;
 import net.devstudy.resume.domain.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
