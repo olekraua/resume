47878311abbfa214b7c5d63e41045bc69ec1ed59
olekraua <ksanya7@gmail.com>
2025-09-27 14:49:06 +0200

    Commit

--- src/main/java/net/devstudy/resume/validator/HtmlSanitized.java
+++ src/main/java/net/devstudy/resume/validator/HtmlSanitized.java
@@ -3,7 +3,11 @@ package net.devstudy.resume.validator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Documented;
 
 @Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
