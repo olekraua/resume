8e4f41de4a0bc161c313120ee0cf5dae826ded2f
olekraua <ksanya7@gmail.com>
2025-09-27 20:08:41 +0200

    Commit

---
+++ src/main/java/net/devstudy/resume/validator/AdulthoodConstraintValidator.java
@@ -0,0 +1,31 @@
+package net.devstudy.resume.validator;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import net.devstudy.resume.annotation.constraints.Adulthood;
+
+public class AdulthoodConstraintValidator implements ConstraintValidator<Adulthood, Date> {
+    private int adulthoodAge;
+
+    @Override
+    public void initialize(Adulthood constraintAnnotation) {
+        this.adulthoodAge = constraintAnnotation.adulthoodAge();
+    }
+
+    @Override
+    public boolean isValid(Date value, ConstraintValidatorContext context) {
+        if (value == null)
+            return true;
+
+        LocalDate birthDate = value.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+
+        return !birthDate.isAfter(LocalDate.now().minusYears(adulthoodAge));
+    }
+
+}
