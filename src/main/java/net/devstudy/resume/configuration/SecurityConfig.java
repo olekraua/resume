347d32858c065a0aea4dcafebf0d1383319dfc7c
olekraua <ksanya7@gmail.com>
2025-10-07 21:44:35 +0200

    Commit

--- src/main/java/net/devstudy/resume/configuration/SecurityConfig.java
+++ src/main/java/net/devstudy/resume/configuration/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-//import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -41,17 +40,6 @@ public class SecurityConfig {
                 this.persistentTokenRememberMeService = persistentTokenRememberMeService;
                 this.accessDeniedHandler = accessDeniedHandler;
         }
-
-        /*
-         * @Bean
-         * public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-         * http
-         * .authorizeHttpRequests(auth -> auth
-         * .anyRequest().authenticated())
-         * .formLogin(Customizer.withDefaults());
-         * return http.build();
-         * }
-         */
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
