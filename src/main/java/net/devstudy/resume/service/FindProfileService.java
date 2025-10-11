41142d33765b07629d08d8d16a94309eaaf34575
olekraua <ksanya7@gmail.com>
2025-10-11 20:05:21 +0200

    Commit

--- src/main/java/net/devstudy/resume/service/FindProfileService.java
+++ src/main/java/net/devstudy/resume/service/FindProfileService.java
@@ -1,3 +1,12 @@
  package net.devstudy.resume.service;
 
+import net.devstudy.resume.domain.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface FindProfileService {
+    Profile findByUid(String uid);
+    Page<Profile> findAll(Pageable pageable);
+    Page<Profile> findBySearchQuery(String query, Pageable pageable);
+}
 
