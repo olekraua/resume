10492be08d9eb39f7402959b34306a1f3bec1ed9
olekraua <ksanya7@gmail.com>
2025-10-11 14:37:42 +0200

    Commit

--- src/main/java/net/devstudy/resume/domain/RememberMeToken.java
+++ src/main/java/net/devstudy/resume/domain/RememberMeToken.java
@@ -3,18 +3,8 @@ package net.devstudy.resume.domain;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
 
-/**
- * MongoDB document for Spring Security persistent remember-me token.
- */
-@Document(collection = "rememberMeToken")
-    // унікальний індекс по 'series'
-    @CompoundIndex(name = "series_unique_idx", def = "{ 'series': 1 }", unique = true)
-    // неунікальний індекс по 'username'
-    @CompoundIndex(name = "username_idx", def = "{ 'username': 1 }")
 public class RememberMeToken extends PersistentRememberMeToken {
 
     @Id
