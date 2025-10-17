19791625ae41e8e0edaa9f44e9433b64725ebd29
olekraua <ksanya7@gmail.com>
2025-10-17 11:11:50 +0200

    Commit

--- src/main/java/net/devstudy/resume/entity/AbstractEntity.java
+++ src/main/java/net/devstudy/resume/entity/AbstractEntity.java
@@ -1,6 +1,7 @@
 package net.devstudy.resume.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.MappedSuperclass;
 import net.devstudy.resume.model.AbstractModel;
@@ -9,6 +10,23 @@ import net.devstudy.resume.model.AbstractModel;
 public abstract class AbstractEntity<T> extends AbstractModel implements Serializable {
 
     private static final long serialVersionUID = 8982713310863621560L;
+
+    public abstract T getId();
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof AbstractEntity<?> other))
+            return false;
+        return Objects.equals(getId(), other.getId());
+
+    }
 
     
 }
