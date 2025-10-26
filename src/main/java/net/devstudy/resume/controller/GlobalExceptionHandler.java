7f3650b930c7b7d1555fa324791d3ba974291a65
olekraua <ksanya7@gmail.com>
2025-10-26 22:27:17 +0100

    Commit

---
+++ src/main/java/net/devstudy/resume/controller/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package net.devstudy.resume.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+/**
+ * Centralizes custom error handling for MVC controllers.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({NoHandlerFoundException.class, NoResourceFoundException.class})
+    public ModelAndView handleNotFound(Exception ex, HttpServletRequest request) {
+        return buildNotFoundView(request, ex.getMessage());
+    }
+
+    private ModelAndView buildNotFoundView(HttpServletRequest request, String message) {
+        ModelAndView mav = new ModelAndView("error/page-not-found");
+        mav.setStatus(HttpStatus.NOT_FOUND);
+        mav.addObject("path", request.getRequestURI());
+        mav.addObject("method", request.getMethod());
+        mav.addObject("message", message);
+        return mav;
+    }
+}
