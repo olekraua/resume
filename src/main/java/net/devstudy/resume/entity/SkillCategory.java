cb90c2d1338fa342ff8b9c5f40297bfad20055df
olekraua <ksanya7@gmail.com>
2025-11-24 21:04:40 +0100

    Commit

--- src/main/java/net/devstudy/resume/entity/SkillCategory.java
+++ src/main/java/net/devstudy/resume/entity/SkillCategory.java
@@ -1,2 +1,47 @@
 package net.devstudy.resume.entity;
 
+import java.io.Serial;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "skill_category")
+public class SkillCategory extends AbstractEntity<Long> {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @SequenceGenerator(name = "SKILL_CATEGORY_ID_GENERATOR", sequenceName = "skill_category_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SKILL_CATEGORY_ID_GENERATOR")
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String category;
+
+    public SkillCategory() {
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
