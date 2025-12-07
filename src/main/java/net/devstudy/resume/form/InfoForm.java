ba7a6dca74ce6c5e24be66144e36afe967b61ac3
olekraua <ksanya7@gmail.com>
2025-12-07 02:29:46 +0100

    Commit

--- src/main/java/net/devstudy/resume/form/InfoForm.java
+++ src/main/java/net/devstudy/resume/form/InfoForm.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import net.devstudy.resume.annotation.constraints.Adulthood;
 
 @Getter
 @Setter
 public class InfoForm {
+    @Adulthood
     private Date birthDay;
 
     @Size(max = 100)
