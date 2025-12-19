9bca7e1a242eb403db3984a3ec815e57a8a22e9b
olekraua <ksanya7@gmail.com>
2025-12-19 23:30:26 +0100

    Commit

--- src/main/java/net/devstudy/resume/component/ImageFormatConverter.java
+++ src/main/java/net/devstudy/resume/component/ImageFormatConverter.java
@@ -1,2 +1,11 @@
 package net.devstudy.resume.component;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.springframework.lang.NonNull;
+
+public interface ImageFormatConverter {
+
+    void convert(@NonNull Path sourceImageFile, @NonNull Path destImageFile) throws IOException;
+}
--- src/main/java/net/devstudy/resume/component/ImageOptimizator.java
+++ src/main/java/net/devstudy/resume/component/ImageOptimizator.java
@@ -1,2 +1,10 @@
 package net.devstudy.resume.component;
 
+import java.nio.file.Path;
+
+import org.springframework.lang.NonNull;
+
+public interface ImageOptimizator {
+
+    void optimize(@NonNull Path image);
+}
