34a53f36dd7f8b51d9de280260535104f87f26ec
olekraua <ksanya7@gmail.com>
2025-12-21 00:12:38 +0100

    Commit

--- src/test/java/net/devstudy/resume/config/SecurityConfigTest.java
+++ src/test/java/net/devstudy/resume/config/SecurityConfigTest.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @WebMvcTest(controllers = SecurityConfigTest.TestController.class)
 @Import({
         SecurityConfig.class,
+        UiProperties.class,
         SecurityConfigTest.SecurityTestConfig.class,
         SecurityConfigTest.TestController.class
 })
