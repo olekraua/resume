fc13a79f0853c7167e95477531ca61b71cf37733
olekraua <ksanya7@gmail.com>
2025-12-20 18:07:01 +0100

    Commit

--- src/test/java/net/devstudy/resume/component/impl/DataBuilderImplTest.java
+++ src/test/java/net/devstudy/resume/component/impl/DataBuilderImplTest.java
@@ -2,7 +2,6 @@ package net.devstudy.resume.component.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +12,7 @@ class DataBuilderImplTest {
     private DataBuilderImpl dataBuilder;
 
     @BeforeEach
+    @SuppressWarnings("unused")
     void setUp() {
         dataBuilder = new DataBuilderImpl(new StubTranslitConverter());
     }
@@ -40,10 +40,10 @@ class DataBuilderImplTest {
 
     @Test
     void buildProfileUidTruncatesToMaxLength() {
-        String uid = dataBuilder.buildProfileUid("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        String uid = dataBuilder.buildProfileUid("A".repeat(40), "B".repeat(40));
 
-        assertEquals(50, uid.length());
-        assertTrue(uid.startsWith("abcdefghijklmnopqrstuvwxyz-"));
+        assertEquals(64, uid.length());
+        assertTrue(uid.startsWith("a".repeat(40) + "-"));
     }
 
     @Test
@@ -51,6 +51,83 @@ class DataBuilderImplTest {
         String uid = dataBuilder.buildProfileUid("@@@", "###");
 
         assertEquals("", uid);
+    }
+
+    @Test
+    void buildProfileUidTrimsLeadingAndTrailingDelimiters() {
+        String uid = dataBuilder.buildProfileUid(" -John- ", " --Doe-- ");
+
+        assertEquals("john-doe", uid);
+    }
+
+    @Test
+    void buildRestoreAccessLinkConcatenatesWithRestorePath() {
+        String link = dataBuilder.buildRestoreAccessLink("http://example.com", "token123");
+
+        assertEquals("http://example.com/restore/token123", link);
+    }
+
+    @Test
+    void buildRestoreAccessLinkKeepsTrailingSlashBehavior() {
+        String link = dataBuilder.buildRestoreAccessLink("http://example.com/", "token123");
+
+        assertEquals("http://example.com//restore/token123", link);
+    }
+
+    @Test
+    void rebuildUidWithRandomSuffixUsesRequestedLengthAndAlphabet() {
+        String baseUid = "john-doe";
+        String alphabet = "abc";
+
+        String uid = dataBuilder.rebuildUidWithRandomSuffix(baseUid, alphabet, 8);
+
+        assertTrue(uid.startsWith(baseUid + "-"));
+        assertEquals(baseUid.length() + 1 + 8, uid.length());
+        String suffix = uid.substring(baseUid.length() + 1);
+        for (int i = 0; i < suffix.length(); i++) {
+            assertTrue(alphabet.indexOf(suffix.charAt(i)) >= 0);
+        }
+    }
+
+    @Test
+    void rebuildUidWithRandomSuffixKeepsTrailingDelimiterWhenCountIsNotPositive() {
+        String uid = dataBuilder.rebuildUidWithRandomSuffix("john-doe", "abc", 0);
+
+        assertEquals("john-doe-", uid);
+    }
+
+    @Test
+    void buildCertificateNameReturnsEmptyForNullOrBlank() {
+        assertEquals("", dataBuilder.buildCertificateName(null));
+        assertEquals("", dataBuilder.buildCertificateName(""));
+    }
+
+    @Test
+    void buildCertificateNameStripsExtensionAndCapitalizes() {
+        String name = dataBuilder.buildCertificateName("file.pdf");
+
+        assertEquals("File", name);
+    }
+
+    @Test
+    void buildCertificateNameReplacesSeparatorsWithSpaces() {
+        String name = dataBuilder.buildCertificateName("my_cert-file.pdf");
+
+        assertEquals("My Cert File", name);
+    }
+
+    @Test
+    void buildCertificateNameReturnsEmptyWhenNormalizedIsEmpty() {
+        String name = dataBuilder.buildCertificateName("###.pdf");
+
+        assertEquals("", name);
+    }
+
+    @Test
+    void buildCertificateNameCapitalizesEachWord() {
+        String name = dataBuilder.buildCertificateName("multi word name.pdf");
+
+        assertEquals("Multi Word Name", name);
     }
 
     private static final class StubTranslitConverter implements TranslitConverter {
