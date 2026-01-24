210c77340b7d9b1bd0902d1b1bd5c1b87cf41845
olekraua <ksanya7@gmail.com>
2026-01-24 01:32:28 +0100

    fix(auth): add explicit getters/setters for ChangePasswordForm

--- src/main/java/net/devstudy/resume/auth/api/dto/ChangePasswordForm.java
+++ src/main/java/net/devstudy/resume/auth/api/dto/ChangePasswordForm.java
@@ -2,12 +2,8 @@ package net.devstudy.resume.auth.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 import net.devstudy.resume.shared.validation.annotation.FieldMatch;
 
-@Getter
-@Setter
 @FieldMatch(first = "newPassword", second = "confirmPassword", message = "{password.match}")
 public class ChangePasswordForm {
     @NotBlank
@@ -20,4 +16,28 @@ public class ChangePasswordForm {
     @NotBlank
     @Size(min = 6, max = 100)
     private String confirmPassword;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
